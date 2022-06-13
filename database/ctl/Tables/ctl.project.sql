81b902b3180716bfebd286067638c4d47bb63316
mb101 <themayurbhatia@gmail.com>
2022-06-13 13:49:39 -0400

    updated project_id to moody_projected_id

--- database/ctl/Tables/ctl.project.sql
+++ database/ctl/Tables/ctl.project.sql
@@ -1,14 +1,21 @@
-CREATE TABLE [ctl].[peregrine_project](
+/****** Object:  Table [ctl].[project]    Script Date: 2022-06-13 1:48:47 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [ctl].[project](
 	[id] [int] NOT NULL,
 	[project_name] [varchar](500) NULL,
-	[project_id] [varchar](100) NULL,
+	[moody_project_id] [varchar](100) NULL,
 	[measurement_model] [varchar](500) NULL,
 	[start_date] [date] NULL,
 	[end_date] [date] NULL,
 	[created_by] [varchar](500) NULL,
 	[created_date] [datetime] NULL,
 	[last_updated_by] [varchar](500) NULL,
-	[last_updated_date] [datetime] NULL
+	[last_updated_date] [datetime] NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
