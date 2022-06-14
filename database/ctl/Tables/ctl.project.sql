48dd664b3b26f8b87bee234593902cc1ff7bd0af
SylvainEY <78229327+SylvainEY@users.noreply.github.com>
2022-06-14 09:09:35 -0400

    keep length for metadata column consistent with other tables

--- database/ctl/Tables/ctl.project.sql
+++ database/ctl/Tables/ctl.project.sql
@@ -12,9 +12,9 @@ CREATE TABLE [ctl].[project](
     [measurement_model] [varchar](20) NULL,
     [start_date] [date] NULL,
     [end_date] [date] NULL,
-    [created_by] [varchar](50) NULL,
+    [created_by] [varchar](500) NULL,
     [created_date] [datetime] NULL,
-    [last_updated_by] [varchar](50) NULL,
+    [last_updated_by] [varchar](500) NULL,
     [last_updated_date] [datetime] NULL,
     PRIMARY KEY CLUSTERED
 (
